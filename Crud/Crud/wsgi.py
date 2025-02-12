d2a98d2ae258fa5c2f3defbd187b4eef5c964f96
BastianSandovalR <ataymayo@gmail.com>
2025-02-12 10:46:17 -0300

    Update wsgi.py

--- Crud/Crud/wsgi.py
+++ Crud/Crud/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
-from django.core.wsgi import get_wsgi_application
+# Determina la ruta base del proyecto. En este caso, partimos del directorio actual (donde está wsgi.py),
+# subimos dos niveles para llegar a la carpeta que contiene manage.py y la carpeta gymapp.
+BASE_DIR = Path(__file__).resolve().parent.parent
+# Agrega BASE_DIR al principio del sys.path para que Python encuentre la carpeta gymapp.
+sys.path.insert(0, str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Crud.Crud.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
